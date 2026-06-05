3fe1d1c45e660ff8530dfea18bb41ba916b8eb2a
Victor Alves Bugueno <victor.bugueno@fiap.com.br>
2026-06-04 23:11:47 -0300

    Removing some mocks from each test case

--- Bug.BetterThanYesterday.API.Tests/Commons/MocksCollection.cs
+++ Bug.BetterThanYesterday.API.Tests/Commons/MocksCollection.cs
@@ -2,10 +2,10 @@
 {
 	public class MocksCollection
 	{
-		public string Habits { get; set; }
-		public string Users { get; set; }
-		public string Plans { get; set; }
-		public string PlanMembers { get; set; }
-		public string CheckIns { get; set; }
+		public string Habits { get; set; } = "[]";
+		public string Users { get; set; } = "[]";
+		public string Plans { get; set; } = "[]";
+		public string PlanMembers { get; set; } = "[]";
+		public string CheckIns { get; set; } = "[]";
 	}
 }
