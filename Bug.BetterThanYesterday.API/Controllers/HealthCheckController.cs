62da585288d33534f7807c72065a743cf343f918
Victor Alves Bugueno <victor.bugueno@fiap.com.br>
2026-06-13 11:30:41 -0300

    Created HealthCheck controller and added a simple README with the run command

---
+++ Bug.BetterThanYesterday.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bug.BetterThanYesterday.API.Controllers;
+
+[Route("api/[controller]")]
+[Route("testapi/[controller]")]
+[ApiController]
+public class HealthCheckController()
+	: ControllerBase
+{
+	[HttpGet()]
+	public async Task<IActionResult> CheckHealth()
+	{
+		return StatusCode(StatusCodes.Status200OK, "API is healthy");
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Bug.BetterThanYesterday
+
+## Running API
+
+```sh
+dotnet run --project .\Bug.BetterThanYesterday.API\Bug.BetterThanYesterday.API.csproj --urls "http://0.0.0.0:5018"
+```
